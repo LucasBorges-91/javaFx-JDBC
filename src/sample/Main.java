7d7dcf461ec67feae608196d660f4d7c3d6096dd
_lucasborges <lucas.borges@dbccompany.com.br>
2020-05-29 17:46:43 -0300

    MainView design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-        Parent parent = loader.load();
-        Scene mainScene = new Scene( parent );
+        ScrollPane scrollPane = loader.load();
+        scrollPane.setFitToHeight( true );
+        scrollPane.setFitToWidth( true );
+        Scene mainScene = new Scene( scrollPane );
         primaryStage.setScene( mainScene );
         primaryStage.setTitle("Imob");
         primaryStage.show();
