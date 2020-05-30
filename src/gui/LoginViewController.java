19a54b06d153b6e6a4c84decf233282a8a0728d3
_lucasborges <lucas.borges@dbccompany.com.br>
2020-05-30 13:08:34 -0300

    about view and login view

---
+++ src/gui/LoginViewController.java
@@ -0,0 +1,39 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable {
+
+  @FXML
+  private TextField textFieldUsuario;
+
+  @FXML
+  private PasswordField passwordFieldSenha;
+
+  @FXML
+  private Button buttonEntrar;
+
+  @FXML
+  private Button buttonCadastrar;
+
+  @FXML
+  public void onButtonEntrarAction() {
+    System.out.println("Entrou!");
+  }
+
+  @FXML
+  public void onButtonCadastrarAction() {
+    System.out.println("Cadastrou!");
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+  }
+}
